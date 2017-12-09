17de9bcb751dc1e026768b611cab3142d7621002
TimHyatt707 <timhyatt707@gmail.com>
2017-12-09 15:02:08 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const PORT = parseInt(process.env.PORT, 10) || 8000;
+
+const server = express();
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors());
+
+server.all('*', (res) => {
+  res.sendStatus('405');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`); // eslint-disable-line no-console
+});
+
+module.exports = server;
